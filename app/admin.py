a6ccf2aced0874f87b263567bc6720749e18f81f
EronePeter <144078877+EronePeter@users.noreply.github.com>
2023-09-16 05:17:37 +0300

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Price, Sale, FuelLitre, LitresBeforeNextTopUp
+from .models import Price, Sale, FuelLitre, LitresBeforeNextTopUp, Expense
 
 admin.site.register(Sale)
 admin.site.register(Price)
 admin.site.register(FuelLitre)
 admin.site.register(LitresBeforeNextTopUp)
+admin.site.register(Expense)
