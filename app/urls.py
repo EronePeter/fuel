733e12abf516bd55b209aed56a2fd6eb580255dd
EronePeter <144078877+EronePeter@users.noreply.github.com>
2023-09-16 05:12:01 +0300

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, expenses
 
 urlpatterns = [
     path('', home, name="home"),
+    path('expenses/', expenses, name="expenses")
 ]
